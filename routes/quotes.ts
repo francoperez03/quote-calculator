ae76270b3461b417c636a93aa5ee11750fd982d6
Franco Perez <franco.perez03@gmail.com>
2023-01-21 18:04:28 -0300

    Added validation params

--- routes/quotes.ts
+++ routes/quotes.ts
@@ -1,15 +1,36 @@
 import express, { Request, Response, NextFunction } from "express";
 import { SuccessResponse } from "../utils/api-response";
 import { NotFoundError } from "utils/api-error";
+import { celebrate, Joi, Segments, errors } from "celebrate";
+
 const router = express.Router();
 
-router.get("/", async (req: Request, res: Response) => {
-  const { pairName } = req.params;
-  return new SuccessResponse("success", {}).send(res);
-});
-router.get("/prices", async (req: Request, res: Response) => {
-  const { pairName, operation, amount } = req.params;
-  return new SuccessResponse("success", {}).send(res);
-});
-
+router.get(
+  "/",
+  celebrate({
+    [Segments.QUERY]: {
+      pairName: Joi.string().required(),
+    },
+  }),
+  async (req: Request, res: Response) => {
+    const { pairName } = req.query;
+    console.log(pairName);
+    return new SuccessResponse("Success", {}).send(res);
+  }
+);
+router.get(
+  "/prices",
+  celebrate({
+    [Segments.QUERY]: {
+      pairName: Joi.string().required(),
+      operation: Joi.string().required(),
+      amount: Joi.number().required(),
+    },
+  }),
+  async (req: Request, res: Response) => {
+    const { pairName, operation, amount } = req.params;
+    return new SuccessResponse("Success", {}).send(res);
+  }
+);
+router.use(errors());
 export default router;
